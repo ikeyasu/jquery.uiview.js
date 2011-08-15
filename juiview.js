797d22ea2e98ce62c3ee290c75210a263faaf090
IKEUCHI Yasuki <ikeyasu@gmail.com>
2011-08-14 18:54:39 -0700

    impl loadViwe

--- juiview.js
+++ juiview.js
@@ -1,6 +1,17 @@
 (function($) {
-  $.loadView = function(path) {
+  var REGEXP_IFDEV = new RegExp('<!--juiview: *ifdev *-->([\n]*?.*?)*?<!--juiview: *end *-->', 'm');
+  $.loadView = function(path, callback) {
     $.get(path, function(res) {
+      if (res)
+        res = res.replace(REGEXP_IFDEV, "");
+      if (callback)
+        callback(res);
+    });
+  }
+  $.fn.loadView = function(path) {
+    var self = this;
+    $.loadView(path, function(res) {
+      $(self).html(res);
     });
   }
 })(jQuery);
